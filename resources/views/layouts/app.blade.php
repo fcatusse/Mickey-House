68fd5249760bb6c6ce4fc199ded8a68cf0cc5264
unknown <habib.redissi@coding-academy.fr>
2019-01-28 18:46:32 +0100

    fix banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans:400,600,700,800" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="css/custom.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 </head>
 
 <body>
