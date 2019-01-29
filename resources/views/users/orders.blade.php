8e897e4fd5878944ffb9b627b0da8644c6814fc4
Habib REDISSI <habib.redissi@gmail.com>
2019-01-29 10:37:20 +0100

    Habibx (#39)
    * Bootstrap & design adapation for the Create dish page
    
    * Design de la page Mes commandes
    
    * Design ameliorations

--- resources/views/users/orders.blade.php
+++ resources/views/users/orders.blade.php
@@ -9,7 +9,6 @@
   @if (count($orders) > 0)
 
     <div class="card-deck">
-
       @foreach($orders as $order)
       <div class="card">
             @isset ($order->photos[0])
@@ -27,22 +26,6 @@
                 <div class="priceHome badge badge-primary">Prix total : {{$order->price}} €</div>
             </div>
         </div>
-{{-- 
-        <div class="card" style="padding:10px;">
-          <h4 class="card-title">{{ $order->name }}</h4>
-          <p class="card-text">Passée le {{$order->created_at}}</p>
-          <img class="card-img-top" src="/img/{{ $order->photos[0] }}" alt="Card image">
-          <div class="card-block">
-          </br>
-            <p class="card-text">{{ $order->description }}</p>
-            <p>Nombre de parts commandées : {{ $order->nb_servings }}</p>
-            <p>Prix total : {{ $order->price }}</p>
-            <p>
-              <a class="btn btn-primary" href="{{route('dish.show', $order->dish_id)}}">Commander de nouveau</a>
-            </p>
-          </div>
-        </div> --}}
-
     @endforeach
   </div>
 
