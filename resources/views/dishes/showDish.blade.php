b93114dcf9aa68f212acabd255b18214ab433947
fcatusse <francois.catusse@gmail.com>
2019-02-01 16:18:19 +0100

    search engine

--- resources/views/dishes/showDish.blade.php
+++ resources/views/dishes/showDish.blade.php
@@ -2,125 +2,113 @@
 
 @section('content')
 
-  @php ($cat = $dish[0]->categories)
-  @php ($photos = $dish[0]->photos)
+    @php ($cat = $dish[0]->categories)
+    @php ($photos = $dish[0]->photos)
 
-{{-- Display the dish information --}}
+    {{-- Display the dish information --}}
 
-<div class="row">
-    <div class="col">
-      <div class="card">
-            @isset ($dish[0]->photos[0])
-            <img class="card-img-top" style="" src="{{ url('/storage/'.$dish[0]->photos[0]) }}">
-            @endisset
-            <div class="card-body">
-                <span class="badge badge-secondary badgeHome">{{ $dish[0]->nb_servings }} Parts disponibles</span>
-                <h5 class="card-title titleHome">{{$dish[0]->name}}</h5>
-                <h6 class="text-muted madeBy"><span>Cuisiné par </span><a href="/users/show/{{$dish[0]->cook_id}}">{{$dish[0]->username}}</a></h6>
-                <p class="card-text descriptionHome">{{$dish[0]->description}}</p>
-            </div>
-            <div class="card-footer catFooter">
-                {{-- <b class="categorieHome">Catégories :</b> --}}
-                @foreach($cat as $categorie)
-                <small class="text-muted">{{ $categorie->title }} - </small>
-                @endforeach
-                <div class="priceHome badge badge-primary">{{$dish[0]->price}} € / Part</div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                @isset ($dish[0]->photos[0])
+                    <img class="card-img-top" style="" src="{{ url('/storage/'.$dish[0]->photos[0]) }}">
+                @endisset
+                <div class="card-body">
+                    <span class="badge badge-secondary badgeHome">{{ $dish[0]->nb_servings }} Parts disponibles</span>
+                    <h5 class="card-title titleHome">{{$dish[0]->name}}</h5>
+                    <h6 class="text-muted madeBy"><span>Cuisiné par </span><a href="/users/show/{{$dish[0]->cook_id}}">{{$dish[0]->username}}</a></h6>
+                    <p class="card-text descriptionHome">{{$dish[0]->description}}</p>
+                </div>
+                <div class="card-footer catFooter">
+                    {{-- <b class="categorieHome">Catégories :</b> --}}
+                    @foreach($cat as $categorie)
+                        <small class="text-muted">{{ $categorie->title }} - </small>
+                    @endforeach
+                    <div class="priceHome badge badge-primary">{{$dish[0]->price}} € / Part</div>
+                </div>
             </div>
         </div>
+        <div class="col">
+            {{-- Display the dish order part : if there are servings available show the form otherwise display an info message "unavailable "--}}
+            <div class="orderCol">
+                @if ($dish[0]->nb_servings > 0 && isset(Auth::user()->id))
+
+                    {!! Form::open(['action' => 'OrderController@storeAndUpdate', 'method'=>'POST']) !!}
+
+                    <div class="form-group">
+                        {{form::label('nb_servings', __('Nombre de parts'))}}
+                        {{form::select('nb_servings', $servings, null, ['class' => 'form-control'])}}
+                    </div>
+
+                    {{ Form::hidden('user_id', Auth::user()->id) }}
+                    {{ Form::hidden('dish_id', $dish[0]->id) }}
+                    {{ Form::hidden('price', $dish[0]->price) }}
+
+                    <div class="form-group">
+                        {{ Form::button('<i class="fas fa-shopping-cart"></i> Commander', ['class' => 'btn btn-success btnHome', 'type' => 'submit']) }}
+                    </div>
+
+                    {!! Form::close() !!}
+                @elseif (isset(Auth::user()->id))
+                    <div class="alert alert-warning" role="alert" style="heigth:30px">
+                        <strong>Désolé...</strong> Ce plat n'est pas disponible pour le moment.
+                    </div>
+                @else
+                    <div class="alert alert-warning" role="alert" style="heigth:30px">
+                        <strong>Désolé...</strong> Vous devez être connecté pour commander
+                    </div>
+                @endif
+            </div>
+            <div class="my-4" id="map" style='width: 400px; height: 300px; margin:auto'></div>
+        </div>
+
     </div>
-    <div class="col">
-      {{-- Display the dish order part : if there are servings available show the form otherwise display an info message "unavailable "--}}
-    <div class="orderCol">
-      @if ($dish[0]->nb_servings > 0 && isset(Auth::user()->id))
 
-      {!! Form::open(['action' => 'OrderController@storeAndUpdate', 'method'=>'POST']) !!}
-
-      <div class="form-group">
-        {{form::label('nb_servings', __('Nombre de parts'))}}
-        {{form::select('nb_servings', $servings, null, ['class' => 'form-control'])}}
-      </div>
-
-      {{ Form::hidden('user_id', Auth::user()->id) }}
-      {{ Form::hidden('dish_id', $dish[0]->id) }}
-      {{ Form::hidden('price', $dish[0]->price) }}
-
-      <div class="form-group">
-      {{ Form::button('<i class="fas fa-shopping-cart"></i> Commander', ['class' => 'btn btn-success btnHome', 'type' => 'submit']) }}
-      </div>
-
-      {!! Form::close() !!}
-    @elseif (isset(Auth::user()->id))
-      <div class="alert alert-warning" role="alert" style="heigth:30px">
-          <strong>Désolé...</strong> Ce plat n'est pas disponible pour le moment.
-      </div>
-    @else
-      <div class="alert alert-warning" role="alert" style="heigth:30px">
-          <strong>Désolé...</strong> Vous devez être connecté pour commander
-      </div>
-    @endif
-    </div>
-      <div class="my-4" id="map3" style='width: 400px; height: 300px; margin:auto'></div>
+    <div class="my-4">
+        @if (count($recommendations) > 0)
+            <h3>Autres plats de ce cuisinier</h3>
+            <div class="card-deck my-4">
+                @for ($i = 1; $i <= count($recommendations); $i++)
+                    <div class="card" style="max-width: 24rem;">
+                        @isset ($recommendations[$i-1]->photos[0])
+                            <img class="card-img-top" style="" src="{{ url('/storage/'.$recommendations[$i-1]->photos[0]) }}">
+                        @endisset
+                        <div class="card-body">
+                            <span class="badge badge-secondary badgeHome">{{ $recommendations[$i-1]->nb_servings }} Parts disponibles</span>
+                            <h5 class="card-title titleHome">{{$recommendations[$i-1]->name}}</h5>
+                            <p class="card-text descriptionHome">{{$recommendations[$i-1]->description}}</p>
+                            <a class="btn btn-primary text-white btnHome" href="{{ "/dish/" . $recommendations[$i-1]->id }}">Voir le plat</a>
+                            <span class="priceHome badge badge-success">{{$recommendations[$i-1]->price}} € / Part</span>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        @endif
     </div>
 
-  </div>
+    <script type="text/javascript">
+        // On initialise la latitude et la longitude de Paris (centre de la carte)
+        var lat = {{$dish[0]->lat}};
+        var lon = {{$dish[0]->long}};
+        var macarte = null;
+        // Fonction d'initialisation de la carte
+        function initMap() {
+            // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+            macarte = L.map('map').setView([lat, lon], 11);
+            var marker = L.marker([lat, lon]).addTo(macarte);
+            // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                // Il est toujours bien de laisser le lien vers la source des données
+                attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                minZoom: 15,
+            }).addTo(macarte);
+        }
 
-  <div class="my-4">
-    @if (count($recommendations) > 0)
-      <h3>Autres plats de ce cuisinier</h3>
-      <div class="card-deck my-4">
-        @for ($i = 1; $i <= count($recommendations); $i++)
-              <div class="card" style="max-width: 24rem;">
-                  @isset ($recommendations[$i-1]->photos[0])
-                  <img class="card-img-top" style="" src="{{ url('/storage/'.$recommendations[$i-1]->photos[0]) }}">
-                  @endisset
-                  <div class="card-body">
-                      <span class="badge badge-secondary badgeHome">{{ $recommendations[$i-1]->nb_servings }} Parts disponibles</span>
-                      <h5 class="card-title titleHome">{{$recommendations[$i-1]->name}}</h5>
-                      <p class="card-text descriptionHome">{{$recommendations[$i-1]->description}}</p>
-                      <a class="btn btn-primary text-white btnHome" href="{{ "/dish/" . $recommendations[$i-1]->id }}">Voir le plat</a>
-                      <span class="priceHome badge badge-success">{{$recommendations[$i-1]->price}} € / Part</span>
-                  </div>
-              </div>
-        @endfor
-      </div>
-    @endif
-  </div>
-  <div id="full_address" style="visibility: hidden"> {{$full_address}}
-  </div>
-
-
-
-    <script>
-    // Initialize and add the map
-    function initMap() {
-    // initialize
-    var paris = {lat: 48.866667, lng: 2.333333};
-    // The map, centered on paris
-    var map = new google.maps.Map(
-        document.getElementById('map3'), {zoom: 12, center: paris});
-    // The marker, positioned at the center
-    var marker = new google.maps.Marker({position: paris, map: map});
-    codeAddress();
-    }
-
-    function codeAddress() {
-    var geocoder;
-    var address = document.getElementById('full_address').value;
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-    }
+        window.onload = function(){
+            initMap();
+        };
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=MYKEY&callback=initMap"
-    async defer></script>
+
+
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,65 +17,73 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans:400,600,700,800" rel="stylesheet" type="text/css">
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <!-- Nous chargeons les fichiers CDN de Leaflet. Le CSS AVANT le JS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+          crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+            crossorigin=""></script>
+
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Mickey House
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Mickey House
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Les plats</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Top 10</a>
-                        </li>
-                        <form class="form-inline my-2 my-lg-0 ml-5">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Rechercher un plat...">
-                            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
-                        </form>
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dishes/map">Autour de moi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Top 10</a>
+                    </li>
+                    <form action="/dishes/search" method="POST" class="form-inline my-2 my-lg-0 ml-5">
+                        @csrf
+                        <input class="form-control mr-sm-2" type="text" name="keyword" placeholder="Rechercher un plat...">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+                    </form>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
                         @endif
-                        @else
+                    @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false" v-pre>
+                               aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->username }} <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 @if (Auth::user()->is_admin)
-                                <a href="{{route('adminPanel')}}" class="dropdown-item">Admin Panel</a>
+                                    <a href="{{route('adminPanel')}}" class="dropdown-item">Admin Panel</a>
                                 @endif
                                 <a href="{{route('create.dish')}}" class="dropdown-item">Ajouter un plat</a>
                                 <a href="{{route('user.show', Auth::user()->id)}}" class="dropdown-item">Ma page</a>
@@ -93,38 +101,38 @@
                                 </form>
                             </div>
                         </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4 font-weight-bold">Mickey House</h1>
-                <p class="lead font-weight-bold">La vente de plats entre particuliers.</p>
+                    @endguest
+                </ul>
             </div>
         </div>
-        <div class="flash-message container mt-4">
-            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-            @if(Session::has('alert-' . $msg))
-            <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}</p>
-            @endif
-            @endforeach
+    </nav>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4 font-weight-bold">Mickey House</h1>
+            <p class="lead font-weight-bold">La vente de plats entre particuliers.</p>
         </div>
-
-        <main class="container py-4">
-            @yield('content')
-
-            <hr class="featurette-divider mt-5">
-
-            <!-- FOOTER -->
-            <footer class="container mt-5">
-                <p class="float-right"><a href="#">Back to top</a></p>
-                <p class="text-center">&copy; 2018-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-            </footer>
-
-        </main>
     </div>
+    <div class="flash-message container mt-4">
+        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+            @if(Session::has('alert-' . $msg))
+                <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}</p>
+            @endif
+        @endforeach
+    </div>
+
+    <main class="container py-4">
+        @yield('content')
+
+        <hr class="featurette-divider mt-5">
+
+        <!-- FOOTER -->
+        <footer class="container mt-5">
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <p class="text-center">&copy; 2018-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+
+    </main>
+</div>
 </body>
 
 </html>
